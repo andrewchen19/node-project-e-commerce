d89ea2719117e4ce9aa7d58b8bf64a3772ec2cf8
Andrew Chen <andrew831119work@gmail.com>
2023-12-21 22:33:18 +0800

    second commit

--- app.js
+++ app.js
@@ -22,16 +22,6 @@ const fileUpload = require("express-fileupload");
 const helmet = require("helmet");
 const { xss } = require("express-xss-sanitizer");
 const rateLimit = require("express-rate-limit");
-const rateLimit = require("express-rate-limit");
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 60, // Limit each IP to 60 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  message: {
-    msg: "Too many requests from this IP, please try again after 15 minutes",
-  },
-});
 
 // middlewares
 app.use(express.json());
@@ -44,7 +34,17 @@ app.use(cookieParser(process.env.JWT_SECRET)); // secret string
 app.use(fileUpload());
 app.use(helmet());
 app.use(xss());
-app.use(limiter());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 60, // Limit each IP to 60 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: {
+      msg: "Too many requests from this IP, please try again after 15 minutes",
+    },
+  })
+);
 
 // routes
 app.use("/api/v1/auth", authRoutes);
